d1b1456ca3390a702d02c50f9541f3a44bb5ed52
GUILLAUME BARATIER <Guillaume.BARATIER1@ibm.com>
2025-01-06 16:07:57 +0100

    clean

--- pages/3_Prompt_with_WatsonX.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("💬 Prompt With WatsonX")
-st.caption("🚀 My First Prompt using WatsonX")
-st.markdown(
-        """
-        On this page we are able to send request to WatsonX and retrive answers
-        """
-        )
